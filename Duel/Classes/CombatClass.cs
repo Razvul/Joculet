d3d4884ccd8825f4af2dd7b96dbdb2b4b17da04a
Razvan Agafitei <razvule@gmail.com>
2021-07-22 16:55:13 +0300

    Add properties for CombatClass

---
+++ Duel/Classes/CombatClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class CombatClass
+    {
+        private int HP;
+        private int Damage;
+        private string Name;
+
+        public CombatClass(int objHP, int objDamage, string objName)
+        {
+            HP = objHP;
+            Damage = objDamage;
+            Name = objName;
+        }
+
+        public void Attack()
+        {
+
+        }
+    }
+}
