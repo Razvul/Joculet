7c320987a6d81e216bd7c7072d7332b01ec58c6e
Razvan Agafitei <razvule@gmail.com>
2021-07-23 21:33:06 +0300

    Added weapons for combat

---
+++ Duel/Classes/Weapons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Weapons
+    {
+        public int[] Gorehowl = { 7, 15 };
+        public int[] Warbreaker = { 5, 11 };
+        public int[] LightsVengeance = { 6, 16 };
+    }
+}
