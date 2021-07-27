c44c0f9f0eb41cd82fb03ebd313ca70e279d9e47
Razvan Agafitei <razvule@gmail.com>
2021-07-27 13:29:07 +0300

    Schimbat prop in propfull

--- Duel/Classes/CombatClass.cs
+++ Duel/Classes/CombatClass.cs
@@ -8,11 +8,52 @@ namespace Classes
 {
     public class CombatClass
     {
-        public int HP;
-        public int MinDamage;
-        public int MaxDamage;
-        public int Armor;
-        public string Name;
+        //public int HP;
+        //public int MinDamage;
+        //public int MaxDamage;
+        //public int Armor;
+        //public string Name;
+
+        private int hp;
+
+        public int HP
+        {
+            get { return hp; }
+            set { hp = value; }
+        }
+
+        private int minDamage;
+
+        public int MinDamage
+        {
+            get { return minDamage; }
+            set { minDamage = value; }
+        }
+
+        private int maxDamage;
+
+        public int MaxDamage
+        {
+            get { return maxDamage; }
+            set { maxDamage = value; }
+        }
+
+        private int armor;
+
+        public int Armor
+        {
+            get { return armor; }
+            set { armor = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
 
         public CombatClass(int objHP, int objMinDamage, int objMaxDamage, int objArmor, string objName)
         {
--- Duel/Classes/Weapons.cs
+++ Duel/Classes/Weapons.cs
@@ -8,8 +8,35 @@ namespace Classes
 {
     public class Weapons
     {
-        public int[] Gorehowl = { 7, 15 };
-        public int[] Warbreaker = { 5, 11 };
-        public int[] LightsVengeance = { 6, 10 };
+        //public int[] Gorehowl = { 7, 15 };
+        //public int[] Warbreaker = { 5, 11 };
+        //public int[] LightsVengeance = { 6, 10 };
+        //public int MinDamage;
+        //public int MaxDamage;
+        //public string WeaponName;
+
+        private int minDamage;
+
+        public int MinDamage
+        {
+            get { return minDamage; }
+            set { minDamage = value; }
+        }
+
+        private int maxDamage;
+
+        public int MaxDamage
+        {
+            get { return maxDamage; }
+            set { maxDamage = value; }
+        }
+
+        private string weaponName;
+
+        public string WeaponName
+        {
+            get { return weaponName; }
+            set { weaponName = value; }
+        }
     }
 }
